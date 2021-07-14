bf4d12ad682272bfbdc1bd8485d7886ee38eee22
Diana Abagyan <36429533+syntaxshill@users.noreply.github.com>
2021-07-14 10:47:51 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name='decision_tree',
+  author='Diana Abagyan',
+  version='1.0.0',
+  description='Creates, visualizes, and evaluates decision tree classifier',
+  packages=['decision_tree']
+)
